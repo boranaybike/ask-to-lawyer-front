44c9b5ce956869e5ceb3ac545cfa5280311018d1
Esen Tekkanat <esentekkanat@gmail.com>
2023-04-28 20:50:57 +0300

    signups

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,6 +26,29 @@
     </signinModal>
   </Teleport>
   </div>
+
+  <div>
+    <v-btn>
+    <button @click="signupLawyerShow = true">Avukat Olarak Kayıt Ol</button>
+    </v-btn>
+    <Teleport to="body">
+    <signupLawyer :show="signupLawyerShow" @close="signupLawyerShow= false">
+
+    </signupLawyer>
+  </Teleport>
+  </div>
+
+  <div>
+    <v-btn>
+    <button @click="signupCustomerShow = true">Danışan Olarak Kayıt Ol</button>
+    </v-btn>
+    <Teleport to="body">
+    <signupCustomer :show="signupCustomerShow" @close="signupCustomerShow = false">
+
+    </signupCustomer>
+  </Teleport>
+  </div>
+
 </template>
 
 
@@ -34,22 +57,27 @@ import { defineComponent } from 'vue';
 import { ref } from 'vue';
 import signinModal from '../components/Signin.vue'
 import signupModal from '../components/Signup.vue'
-
+import signupLawyer from '../components/SignupLawyer.vue'
+import signupCustomer from '../components/SignupCustomer.vue'
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     signinModal,
-    signupModal 
+    signupModal,
+    signupLawyer,
+    signupCustomer
   
       },
       setup() {
     const signinModalShow = ref(false);
     const signupModalShow = ref(false);
+    const signupLawyerShow = ref(false);
+    const signupCustomerShow = ref(false);
   
 
 
-    return { signinModalShow , signupModalShow};
+    return { signinModalShow , signupModalShow , signupLawyerShow , signupCustomerShow};
   },
     
 });
