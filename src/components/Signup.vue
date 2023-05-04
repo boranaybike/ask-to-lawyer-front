245f53e3dbc11334dc903066a9bb88f15ee4b79f
ik3 <aybikeboran7140@gmail.com>
2023-05-05 01:37:36 +0300

    Add Signup.vue

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,63 +1,79 @@
 <template>
   <div class="text-center">
-    <button @click="signup = true">
-      Kayıt Ol
-  </button>
-
-    <v-dialog v-model="signup">
-      <v-card>
-        <v-card-text>
-         <div class="box">
-          <h3>KAYIT OL</h3>
-          <div class="lawyer">
-            <button> Avukat Olarak Kayıt Ol</button>
-            <div class="client">
-              <button> Müşteri Olarak Kayıt Ol</button>
-            </div>
-          </div>
-         </div>
-        </v-card-text>
-        <div class="signup-footer">
-          <slot name="footer">
-            Üyeliğiniz var mı?
-            <button class="signin-button" @click="signup = false" >Giriş Yapın</button>
-          </slot>
-        </div>
-       <!-- <v-card-actions>
-          Üyeliğiniz var mı?
-          <button @click="signup = false">Giriş Yapın</button>
-        </v-card-actions> -->
+    <v-dialog
+      v-model="dialog"
+      width="auto">
+      <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          v-bind="props">
+          Kayıt Ol
+        </v-btn>
+      </template>
+      <v-card class="mx-auto" max-width="500">
+        <v-container fluid>
+          <p class="text-h4 text--primary">KAYIT OL</p>
+          <v-row dense>
+            <v-col :cols="6"> 
+              <v-card>            
+                <v-img
+                  :src="require('@/assets/images/lawyer_signup.jpg')"
+                  class="align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  height="200px"
+                  cover>            
+                <v-card-title class="text-white">Avukat olarak<br> kayıt ol</v-card-title>
+                </v-img>
+              </v-card>
+                  </v-col>
+                  <v-col :cols="6">
+                  <v-card>
+                      <v-img
+                          :src="require('@/assets/images/customer_signup.jpg')"
+                          class="align-end"
+                          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                          height="200px"
+                          cover>
+                      <v-card-title class="text-white">Danışan olarak <br/>kayıt ol</v-card-title>
+                      </v-img>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-card>
     </v-dialog>
   </div>
 </template>
 
-<script>
-  export default{
-    data () {
-      return {
-        signup: false,
-      }
-    },
-  }
+<script lang="ts">
+export default {
+    name: "Signup",
+  components: {
+  },
+  data () {
+    return {
+      dialog:false,
+    }
+  },
+}
 </script>
 
-<style>
-
-.box{
-position: relative;
-width: 914px;
-height: 615px;
-left: 407px;
-top: 313px;
-
-background: #E7ECEF;
-opacity: 0.99;
-border-radius: 30px;
+<style lang="scss" scoped>
+.v-dialog > .v-overlay__content{
+  border-radius:5%!important;
 }
-.signup-footer{
-  margin-top: 490px;
-  margin-left: 115px;
-  color: black;
+.text-h4{
+  text-align: center;
+}
+
+.v-container--fluid{
+  width: 600px;
+}
+.v-responsive{
+  height: 250px!important;
+}
+.text-white{
+  background: rgba(0, 0, 0, 0.5);
+  text-align: center;
 }
 </style>
