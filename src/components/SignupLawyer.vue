9c5dc39547daa876392c695de1e523da8d0d5bfb
Esen Tekkanat <esentekkanat@gmail.com>
2023-05-06 13:46:49 +0300

    signup and sign in popups done

--- src/components/SignupLawyer.vue
+++ src/components/SignupLawyer.vue
@@ -14,33 +14,33 @@ const props = defineProps({
           <h3>Avukat Olarak Kayıt Ol</h3>
         </div>
         <div class="nameSurname">
-            <label for="nameSurname" style="margin-right: 335px; margin-bottom: 10px;">İsim Soyisim</label>
+            <label for="nameSurname" >İsim Soyisim</label>
             <input type="nameSurname" id="nameSurname" v-model="nameSurname" required>
         </div>
 
         <div class="email">
-            <label for="email" style="margin-right: 335px; margin-bottom: 10px;">Email</label>
+            <label for="email" >E-mail</label>
             <input type="email" id="email" v-model="email" required>
         </div>
 
         <div class="barono">
-            <label for="barono" style="margin-right: 335px; margin-bottom: 10px;">Baro Sicil No.</label>
+            <label for="barono" >Baro Sicil No.</label>
             <input type="barono" id="barono" v-model="barono" required>
         </div>
 
         <div class="telphone">
-            <label for="telphone" style="margin-right: 335px; margin-bottom: 10px;">Telefon</label>
+            <label for="telphone">Telefon</label>
             <input type="telphone" id="telphone" v-model="telphone" required>
         </div>
 
 
         <div class="password">
-            <label for="password" style="margin-right: 335px; margin-bottom: 10px;">Şifre</label>
+            <label for="password" >Şifre</label>
             <input type="password" id="password" v-model="password" required>
         </div>
 
-        <div class="password">
-            <label for="password" style="margin-right: 335px; margin-bottom: 10px;">Şifre Tekrar</label>
+        <div class="passwordagain">
+            <label for="password" >Şifre Tekrar</label>
             <input type="password" id="password" v-model="password" required>
         </div>
 
@@ -62,49 +62,65 @@ const props = defineProps({
 
 <style lang="scss" scoped>
 #nameSurname{
-  border: 1px solid #d5d5d5;
+  position: absolute;
   width: 409px;
   height: 37px;
+  left: 4px;
+  top: 35px;
+
+  background: #FFFFFF;
   border-radius: 10px;
-  background-color: white;
-  padding: 8px;
 }
 
 #barono{
-  border: 1px solid #d5d5d5;
+  position: absolute;
   width: 409px;
-  border-radius: 10px;
-  background-color: white;
-  padding: 8px;    
+  height: 37px;
+  left: 4px;
+  top: 35px;
+
+  background: #FFFFFF;
+  border-radius: 10px;  
 }
 
 #telphone{
-  border: 1px solid #d5d5d5;
+  position: absolute;
   width: 409px;
-  border-radius: 10px;
-  background-color: white;
-  padding: 8px;   
+  height: 37px;
+  left: 4px;
+  top: 35px;
+
+  background: #FFFFFF;
+  border-radius: 10px; 
 }
 
 #password{
-  border: 1px solid #d5d5d5;
+  position: absolute;
   width: 409px;
+  height: 37px;
+  left: 4px;
+  top: 35px;
+
+  background: #FFFFFF;
   border-radius: 10px;
-  background-color: white;
-  padding: 8px;
 }
 
 #email{
-  border: 1px solid #d5d5d5;
+  position: absolute;
   width: 409px;
+  height: 37px;
+  left: 4px;
+  top: 35px;
+
+  background: #FFFFFF;
   border-radius: 10px;
-  background-color: white;
-  padding: 8px;
 
 }
 
 .modal-mask {
-
+  position: fixed;
+  justify-content: center;
+  align-items: center;
   z-index: 9998;
   top: 0;
   left: 0;
@@ -115,154 +131,169 @@ const props = defineProps({
 }
 
 .modalContainer {
-position: relative;
-width: 513px;
-height: 849px;
-padding: 20px 30px;
-border-radius: 20px;
-background-image: linear-gradient(rgba(0, 0, 0, 0.477),rgba(0, 0, 0, 0.477)),url(../assets/images/lawyer_signup.jpg);
-opacity: 0.99;
-box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-transition: all 0.3s ease;
-background-size: cover;
+  position: relative;
+  width: 513px;
+  height: 720px;
+  padding: 20px 30px;
+  border-radius: 20px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.477),rgba(0, 0, 0, 0.477)),url(../assets/images/lawyer_signup.jpg);
+  background-position: center;
+  opacity: 0.99;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  background-size: cover;
 
 }
 
 .modalHeader h3 {
-position: absolute;
-width: 358px;
-height: 34px;
-left: 77px;
-top: 48px;
+  position: absolute;
+  width: 358px;
+  height: 34px;
+  left: 77px;
+  top: 48px;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 700;
-font-size: 34px;
-line-height: 41px;
-display: flex;
-align-items: center;
-letter-spacing: -0.02em;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 34px;
+  line-height: 41px;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.02em;
 
-color: #FFFFFF;
+  color: #FFFFFF;
 }
 
 .barono{
-position: absolute;
-width: 145px;
-height: 29px;
-left: 48px;
-top: 311px;
+  position: absolute;
+  width: 145px;
+  height: 29px;
+  left: 48px;
+  top: 311px;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 24px;
-line-height: 29px;
-/* identical to box height */
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 29px;
 
-display: flex;
-align-items: center;
-letter-spacing: -0.02em;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.02em;
 
-color: #FFFFFF;
+  color: #FFFFFF;
 }
 
 .nameSurname{
-position: absolute;
-justify-content: center;
-width: 135px;
-height: 29px;
+  position: absolute;
+  width: 135px;
+  height: 29px;
+  left: 48px;
+  top: 137px;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 24px;
-line-height: 29px;
-/* identical to box height */
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 29px;
 
-display: flex;
-align-items: center;
-letter-spacing: -0.02em;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.02em;
 
-color: #FFFFFF;
+  color: #FFFFFF;
+
 }
 
 .telphone{
-position: absolute;
-width: 81px;
-height: 29px;
-left: 48px;
-top: 390px;
+  position: absolute;
+  width: 81px;
+  height: 29px;
+  left: 48px;
+  top: 390px;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 24px;
-line-height: 29px;
-/* identical to box height */
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 29px;
 
-display: flex;
-align-items: center;
-letter-spacing: -0.02em;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.02em;
 
-color: #FFFFFF;
+  color: #FFFFFF;
 }
 .email {
-position: absolute;
-width: 69px;
-height: 29px;
-left: 48px;
-top: 222px;
+  position: absolute;
+  width: 69px;
+  height: 29px;
+  left: 48px;
+  top: 222px;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 24px;
-line-height: 29px;
-/* identical to box height */
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 29px;
 
-display: flex;
-align-items: center;
-letter-spacing: -0.02em;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.02em;
 
-color: #FFFFFF;
-flex-direction: column;
+  color: #FFFFFF;
+
 }
 
 .password {
-position: absolute;
-width: 51px;
-height: 29px;
-left: 48px;
-top: 480px;
+  position: absolute;
+  width: 51px;
+  height: 29px;
+  left: 48px;
+  top: 480px;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 24px;
-line-height: 29px;
-/* identical to box height */
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 29px;
 
-display: flex;
-align-items: center;
-letter-spacing: -0.02em;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.02em;
 
-color: #FFFFFF;
-
-  flex-direction: column;
+  color: #FFFFFF;
   
+}
+.passwordagain{
+  position: absolute;
+  width: 126px;
+  height: 29px;
+  left: 48px;
+  top: 568px;
+
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 29px;
+
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.02em;
+
+  color: #FFFFFF;
 }
 
 .modal-default-button {
-position: absolute;
-width: 409px;
-height: 66px;
-left: 48px;
-top: 688px;
+  position: absolute;
+  width: 300px;
+  height: 50px;
+  left: 110px;
+  top: 660px;
 
-background: #EC4C0F;
-border-radius: 20px;
+  background: #EC4C0F;
+  border-radius: 20px;
 }
 
 .modal-enter-from {
