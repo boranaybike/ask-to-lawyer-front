22a745a2dfb4ca79471ac9706404d6c23903a506
ik3 <aybikeboran7140@gmail.com>
2023-05-10 02:12:59 +0300

    merge

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <div>
       <signupModal/>
+      <frame18/>
+      <frame17/>
     </div>
 
     <div>
@@ -14,8 +16,7 @@
     </div>
 
     <div>
-      <v-btn>
-      <button @click="signupLawyerShow = true">Avukat Olarak Kayıt Ol</button>
+      <v-btn @click="signupLawyerShow = true">Avukat Olarak Kayıt Ol
       </v-btn>
       <Teleport to="body">
       <signupLawyer :show="signupLawyerShow" @close="signupLawyerShow= false">
@@ -25,8 +26,7 @@
     </div>
 
   <div>
-    <v-btn>
-    <button @click="signupCustomerShow = true">Danışan Olarak Kayıt Ol</button>
+    <v-btn @click="signupCustomerShow = true">Danışan Olarak Kayıt Ol
     </v-btn>
     <Teleport to="body">
     <signupCustomer :show="signupCustomerShow" @close="signupCustomerShow = false">
@@ -48,6 +48,8 @@ import signupModal from '../components/Signup.vue'
 import signupLawyer from '../components/SignupLawyer.vue'
 import signupCustomer from '../components/SignupCustomer.vue'
 import profile from '../components/shared/LawyerProfileEdit.vue'
+import frame18 from '../components/frame18.vue'
+import frame17 from '../components/frame17.vue'
 
 export default defineComponent({
   name: 'HomeView',
@@ -56,7 +58,9 @@ export default defineComponent({
     signupModal,
     signupLawyer,
     signupCustomer,
-    profile
+    profile,
+    frame18,
+    frame17,
   
       },
       setup() {
@@ -64,7 +68,9 @@ export default defineComponent({
     const signupModalShow = ref(false);
     const signupLawyerShow = ref(false);
     const signupCustomerShow = ref(false);
-    return { signinModalShow , signupModalShow , signupLawyerShow , signupCustomerShow};
+    const frame18Show = ref(false);
+    const frame17Show = ref(false);
+    return { signinModalShow , signupModalShow , signupLawyerShow , signupCustomerShow, frame18Show, frame17Show};
   },
     
 });
