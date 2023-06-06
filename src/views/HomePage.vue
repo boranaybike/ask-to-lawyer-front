79c79d4c777e36fa8720824570c6ce0326658ca8
FurkanLuleci <frkn03lll@gmail.com>
2023-06-06 22:36:29 +0300

    .

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,92 +1,118 @@
 <template>
-  <div class="twitter-chatbox">
-    <div class="chatbox-messages">
-      <!-- Mesajları göstermek için bir döngü kullanabilirsiniz -->
-      <div v-for="message in messages" :key="message.id" :class="['message', { 'sent': message.sent }]">
-        <div class="message-content">
-          {{ message.text }}
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="auto"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          v-bind="props"
+        >
+          Open Dialog
+        </v-btn>
+      </template>
+
+      <v-card style="width: 750px;"><v-row>  <v-card-actions>
+          <v-btn  block @click="dialog = false"><v-icon icon="mdi-close"></v-icon></v-btn>
+        </v-card-actions></v-row>
+     <v-layout>
+      <v-navigation-drawer
+       
+        permanent
+        theme="dark"
+      >
+        <v-list nav>
+          <v-list-item style="background-color: rgb(255, 131, 43);" title="Kişi1" value="1"></v-list-item>
+          <v-list-item  title="Kişi2" value="2"></v-list-item>
+          <v-list-item  title="Kişi3" value="3"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 400px"><div class="chat">
+    <div class="chat-container">
+      <div class="chat-messages">
+        <div v-for="message in messages" :key="message.id" class="chat-message">
+          <div class="chat-sender">{{ message.sender }}</div>
+          <div class="chat-text">{{ message.text }}</div>
         </div>
       </div>
+      <div class="chat-input">
+        <v-text-field v-model="newMessage"  variant="solo" label="Message" @keyup.enter="sendMessage" ></v-text-field>
+        <v-btn color="primary" @click="sendMessage">Send</v-btn>
+      </div>
     </div>
-    <div class="chatbox-input">
-      <v-text-field v-model="newMessage" placeholder="Mesajınızı girin" style="background-color: aliceblue;"></v-text-field>
-      <v-btn color="#ec4e0f81">Gönder</v-btn>
-    </div>
+  </div></v-main>
+    </v-layout>
+    
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  data() {
-    return {
-      messages: [
-        { id: 1, text: "Merhaba!", sent: false },
-        { id: 2, text: "Selam!", sent: true },
-        { id: 3, text: "Nasılsın?", sent: false },
-        { id: 4, text: "İyiyim, teşekkürler!", sent: true },
-        { id: 4, text: "İyiyim, teşekkürler!", sent: false },
-      ],
-      newMessage: ''
-    };
-  },
-  // methods: {
-  //   sendMessage() {
-  //     if (this.newMessage) {
-  //       this.messages.push({ id: Date.now(), text: this.newMessage, sent: true });
-  //       this.newMessage = '';
-  //     }
-  //   }
-  // }
-};
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false,
+        messages: [],
+        newMessage: ''
+      }
+    },
+    methods: {
+    sendMessage() {
+      if (this.newMessage) {
+        this.messages.push({
+          id: this.messages.length + 1,
+          sender: 'User',
+          text: this.newMessage
+        });
+        this.newMessage = '';
+      }
+    }
+  }
+  }
 </script>
 
 <style scoped>
-.twitter-chatbox {
-  background-color: #cde7f7;
-  width: 600px;
-  height: 500px;
+.chat {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.chat-container {
+  background-color: #E7ECEF;
+  min-height: 400px;
   border: 1px solid #ccc;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-}
-
-.chatbox-messages {
-  flex: 1;
-  overflow-y: scroll;
   padding: 10px;
 }
 
-.message {
+.chat-messages {
+  
+  max-height: 300px;
+  overflow-y: auto;
+  
+}
+
+.chat-message {
   background-color: #9BA9B9;
-  padding: 10px;
-  margin-bottom: 10px;
   border-radius: 10px;
-  text-align: left;
+  margin-bottom: 10px;
+  width: min-content;
+  min-width: 75px;
 }
 
-.message-content {
-  font-size: 14px;
+.chat-sender {
+  font-weight: bold;
 }
 
-.sent {
-  text-align: right;
-  background-color: #ec4e0f81;
-  align-self: flex-end;
+.chat-text {
+  margin-top: 5px;
 }
 
-.chatbox-input {
+.chat-input {
   display: flex;
-  padding: 10px;
-  align-items: center;
-}
 
-.v-text-field {
-  flex: 1;
-  margin-right: 10px;
-}
-.v-input__details {
-
-  display: none;
+  margin-top: 10px;
+  
 }
 </style>
