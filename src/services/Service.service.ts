0ccfb29ad62aa4062cb947fdf99b31253f99f04e
ik3 <aybikeboran7140@gmail.com>
2023-06-12 18:18:10 +0300

    Customer register operation added.

--- src/services/Service.service.ts
+++ src/services/Service.service.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
+import router from "@/router";
+import tokenService from "./Token.service";
+
 const API_URL = "https://localhost:7057/api";
-
 const axiosInstance = axios.create({baseURL: API_URL});
-
 
 axiosInstance.interceptors.request.use(
     (config) => {
+      config.headers['Authorization'] = `Bearer ${tokenService.getToken()}`;
       config.headers['Content-Type'] = 'application/json';
       return config;
     },
@@ -14,5 +16,21 @@ axiosInstance.interceptors.request.use(
     }
   );
 
-export default axiosInstance;
+  axiosInstance.interceptors.request.use(
+    (response) => response,
+    (error) => { 
+      const originalRequest = error.config;
 
+      if (
+        error.response.status === 401 &&
+        originalRequest.url !== `${API_URL}/users/login`
+      ) {
+        router.push('/login');
+        return Promise.reject(error);
+      }
+  
+      return Promise.reject(error);
+    }
+  );
+
+export default axiosInstance;
---
+++ src/views/Customer/CustmerRegisterView.vue
@@ -0,0 +1,24 @@
+<template>
+
+      <div class="customer-register">
+        <customerSignup/>  
+      </div>
+</template>
+      
+      
+    <script lang="ts">
+    import { defineComponent } from 'vue';
+    import customerSignup from '@/components/SignupCustomer.vue'
+    
+    export default defineComponent({
+      name: 'CustomerRegister',
+      components: { customerSignup },
+      
+      setup() { 
+        return { customerSignup};
+      }
+    });
+    </script>
+    <style lang="scss" scoped>
+    </style>
+    
