3e5a1e67847480fe7a95f650b496de04a73d34dd
ik3 <aybikeboran7140@gmail.com>
2023-06-19 16:26:40 +0300

    added offer operation to db

--- src/components/customer/OffersCard.vue
+++ src/components/customer/OffersCard.vue
@@ -1,27 +1,17 @@
 <template>
     <v-card class="mx-auto my-12"
-      max-width="374">
-     
+      max-width="374">   
     <v-card-item>
            <v-row><v-col cols="6" style="text-align:left; position: absolute;
-  top: -40px;
-  left: 16px; overflow: visible;" > <v-avatar size="75"
-            
-            
-            
+            top: -40px; left: 16px; overflow: visible;" > <v-avatar size="75"
             color="black"
               image="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
             ></v-avatar></v-col> 
               <v-col cols="12" style="text-align:right;"> {{ date }}</v-col> </v-row>
         </v-card-item>
-    
-  
-     
-  
+
       <v-card-item>
-        <v-card-title  align="left">{{ lawyerName }}</v-card-title>
-  
-        
+        <v-card-title  align="left">{{ offerLawyerFirstName }} {{ offerLawyerLastName }}</v-card-title>
       </v-card-item>
   
       <v-card-text>
@@ -46,68 +36,152 @@
         </v-row>
         
   
-        <div class="my-4 text-subtitle-" align="left" >
+        <div class="my-4 text-subtitle-" >
            {{ baro  }}
         </div><div>
         
         <v-row>
         <v-card-subtitle style="padding-right: 0px; padding-left: 12px;">
-          <span class="my-4 " align="left" >{{ category }}</span>
+          <span class="my-4" >{{ category }}</span>
        
           
         </v-card-subtitle> </v-row></div>
   
         <div style="margin-top: 25px;">{{ question }}</div>
       </v-card-text>
-  
-      
-  
-      <v-card-title style="text-align:right">{{ price }} ₺</v-card-title>
-  
-      
-  
-      <frame17/>
+
+      <v-card-title style="text-align:right">{{ offerPrice }} ₺</v-card-title>
+
+      <v-row justify="center">
+        <v-dialog
+          v-model="dialog"
+          persistent
+          width="auto"
+        >
+          <template v-slot:activator="{ props }">
+            <v-btn
+              color="primary"
+              v-bind="props"
+            >
+            teklifi kabul et        
+          </v-btn>
+          </template>
+          <v-card>
+            <v-card-text> Teklifi onaylıyor musunuz?</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="green-darken-1"
+                variant="text"
+                @click="acceptOffer(); dialog=false; dialog2=true"
+              >
+              Onayla
+              </v-btn>
+              <v-btn
+                color="green-darken-1"
+                variant="text"
+                @click="dialog = false; "
+              >
+              Tekliflere Dön
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
         
+      <v-dialog
+        v-model="dialog2"
+        width="auto"
+      >
+        <v-card>
+          <v-card-title>
+            <span>teklif kabul edilmiştir</span>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn
+              color="primary"
+              variant="text"
+              @click="dialog2 = false;"
+            >
+              Tekliflere dön
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      </v-row>
+
+
+
+
+
+
     </v-card>
   </template>
+
   <script>
   import { defineComponent } from 'vue';
   import { ref } from 'vue';
-  import frame17 from '@/components/frame17.vue';
+  import axiosInstance from '@/services/Service.service'
+
   
-    export default defineComponent({
+  export default defineComponent({
 
         name: 'CustomerOffersView',
-         components: { frame17 },
-         setup() {
-        const question = ref("Yolda Giderken adam bana yan baktı ve ben de çekip vurdum kaç yıl yatarım");
-        const lawyerName=ref("Rick Grimes");
-        const price = ref(100);
-        const baro = ref("Antalya Barosu")
+        components: { },
+        setup(props) {
         const category = ref("Ceza Hukuku")
         const rate = ref(3.5)
-        const frame17Show = ref(false);
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const date = ref(new Date().toLocaleDateString('tr-TR', options));
+        const dialog = ref(false);
+        const dialog2 = ref(false);
+        
+        const offerState = ref(props.offerState);
 
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  const date = ref(new Date().toLocaleDateString('tr-TR', options));
+        const acceptOffer = () => {
+          offerState.value = true;
+            
+          const updatedOffer={
+            id: props.offerId,
+            isAccepted: offerState.value
+          }
 
-  setInterval(() => {
-    date.value = new Date().toLocaleDateString('tr-TR', options);
-  }, 1000);
+          const response =  axiosInstance.post("/Offers/Update", updatedOffer);
+        };
+
+        setInterval(() => {
+          date.value = new Date().toLocaleDateString('tr-TR', options);
+        }, 1000);
 
   
-      return { date, question, lawyerName,price, baro , category, rate ,frame17Show};
+      return { date , category, rate, acceptOffer, dialog, dialog2};
     },
-  
-      
-      
-     
+
+    props: {
+          offerPrice: {
+            required: false
+          },
+          offerLawyerFirstName: {
+            required: true
+          },
+          offerLawyerLastName: {
+            required: false
+          },
+          question: {
+            required: false
+          },
+          offerId: {
+            required: true
+          },
+          offerState: {
+            required: true
+          },
+        },
     })
   </script>
   
   <style scoped="scss">
-  
-  
   .v-btn {
    
     color:white;
@@ -119,10 +193,6 @@
     position: relative;
     overflow: visible;
   }
-  
-  
-  
-
   </style>
       
       
--- src/components/lawyer/LawyerOffersCard.vue
+++ src/components/lawyer/LawyerOffersCard.vue
@@ -2,11 +2,11 @@
     <v-card
       class="mx-auto"
       max-width="444"
-      background-color= "#E7ECEF"
-    >
+      background-color= "#E7ECEF">
       <v-card-item>
         <div>
-          <v-row>      <v-col cols="2">
+          <v-row>      
+            <v-col cols="2">
         <v-sheet class="pa-2 ma-2">
             <v-avatar
             display="flex"
@@ -18,36 +18,76 @@
         <v-col>
         <v-sheet class="pa-2 ma-2">
           <div style="color:black; display: flex; justify-content: left; font-size: 13px; width: 90px; height: 18px; left: 167px; top: 282px;">
-
-            Cem Karaca
-          </div>
+            {{offerClientFirstName}}
+            </div>
           <div style="display: flex; justify-content: left; font-size: 8px;">
             5 gün önce soruldu
           </div></v-sheet>
             </v-col>
           </v-row>
         
-          <div style="padding-right: 10px; font-size: 18px;" class="text-caption">“ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam? ”</div>
+          <div style="padding-right: 10px; font-size: 18px;" class="text-caption">
+            “ {{question}} ”</div>
         </div>
 
         <div style="color:black; font-weight: 700; display: flex; padding-top: 10px; padding-left: 12px; font-size: 10px;">
-            100 ₺ Teklif Verildi
+            {{ offerPrice }} ₺ Teklif Verildi
           </div>
       </v-card-item>
   
       <v-card-actions>
+        <div v-if="offerState==false">
         <v-btn color="white" rounded >
             Onay Bekleniyor
-      </v-btn>
+      </v-btn></div>
+      <div v-else>
+      <v-btn color="white" rounded >
+            Cevabınız Bekleniyor
+      </v-btn></div>
+
 
       </v-card-actions>
     </v-card>
   </template>
 
 
+
 <script>
-  export default {}
+import { defineComponent } from 'vue';
+import { ref } from 'vue';
+
+export default defineComponent({
+      components: {  },
+      setup(props) {
+      const category = ref("Ceza Hukuku")
+      const rate = ref(3.5)
+      console.log(props.offerState)
+
+
+    return { category, rate};
+  },
+
+  props: {
+    
+        offerPrice: {
+          required: false
+        },
+        offerClientFirstName: {
+          required: true
+        },
+        offerLawyerBar: {
+          required: false
+        },
+        question: {
+          required: false
+        },
+        offerState:{
+          required:false
+        }
+      },
+  })
 </script>
+
 
 <style lang="scss" scoped>
 
