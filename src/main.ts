92807b91022f3ee41ce56ef32192e27d9cc17cc4
Aybike Boran <60114286+boranaybike@users.noreply.github.com>
2023-10-23 02:12:18 +0300

    install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -20,4 +19,4 @@ const vuetify = createVuetify({
   directives,
 })
 
-createApp(App).use(store).use(router).use(vuetify).mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app')
